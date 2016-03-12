9ade85b2f52de46d2199a07c26c9167d6a85d0f2
Molly McGlone <mollykmcglone@gmail.com>
2016-03-11 16:04:39 -0800

    add scripts file

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+var print = function(whatPrint) {
+  var resultUl = document.getElementById("result");
+  resultUl.innerHTML = whatPrint;
+}
+
+var pingPong = function(userEntry) {
+  var result = "";
+    for (i = 1; i <= userEntry; i ++) {
+      if (i % 3 === 0 && i % 5 === 0) {
+        result += "<li> <strong>pingpong!</strong> </li>";
+      } else if (i % 3 === 0) {
+        result += "<li> <strong>ping!</strong> </li>";
+      } else if (i % 5 === 0) {
+        result += "<li> <strong>pong!</strong> </li>";
+      } else {
+        result += "<li>" + i + "</li>";
+      }
+      print(result);
+    }
+}
+
+$(document).ready(function() {
+  $("#game").submit(function(event) {
+    event.preventDefault();
+    var $entry = parseInt($( "input" ).val());
+    pingPong($entry);
+    $("#result").show();
+  });
+});
